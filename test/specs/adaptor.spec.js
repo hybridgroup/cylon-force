ac47d007174e7c8bb9e2603b7d0291ba752dc9c8
Andrew Stewart <andrew@stwrt.ca>
2014-06-10 14:23:10 -0700

    commands is now an array, rather than a function

--- test/specs/adaptor.spec.js
+++ test/specs/adaptor.spec.js
@@ -6,7 +6,7 @@ describe('Cylon.Adaptors.Force', function() {
   var force = new Adaptor();
 
   it("exposes a 'commands' function containing all adaptor commands", function() {
-    var commands = force.commands();
+    var commands = force.commands;
 
     expect(commands).to.be.a('array');
 
