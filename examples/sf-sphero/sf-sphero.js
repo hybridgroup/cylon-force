5ee4a4af8a3a74421634ff77d5c7b04c48de643e
Andrew Stewart <andrew@stwrt.ca>
2014-11-17 15:19:51 -0800

    Update example

--- examples/sf-sphero/sf-sphero.js
+++ examples/sf-sphero/sf-sphero.js
@@ -1,58 +1,57 @@
-var cylon = require('cylon');
+var Cylon = require('cylon');
 
-var salesforceRobot = {
-  name: 'salesforce',
+Cylon.robot({
+  name: "salesforce",
 
   totalBucks: 0,
 
-  connection: [
-    {
-      name: 'sfcon',
+  connections: {
+    sphero: { adaptor: 'sphero', port: '/dev/rfcomm0' },
+
+    salesforce: {
       adaptor: 'force',
+
       sfuser: process.env.SF_USERNAME,
       sfpass: process.env.SF_SECURITY_TOKEN,
+
       orgCreds: {
         clientId: process.env.SF_CLIENT_ID,
         clientSecret: process.env.SF_CLIENT_SECRET,
         redirectUri: 'http://localhost:3000/oauth/_callback'
       }
-    },
-    { name: 'sphero', adaptor: 'sphero', port: '/dev/rfcomm0' }
-  ],
+    }
+  },
 
-  devices: [
-    { name: 'salesforce', driver: 'force', connection: 'sfcon' },
-    { name: 'sphero', driver: 'sphero', connection: 'sphero' }
-  ],
+  devices: {
+    salesforce: { driver: 'force', connection: 'salesforce' },
+    sphero: { driver: 'sphero', connection: 'sphero' }
+  },
 
-  work: function(robot) {
-    cylon.Logger.info('Setting up Collision Detection...');
-    robot.sphero.detectCollisions();
-    robot.sphero.stop();
-    robot.sphero.setRGB(0x00FF00);
-    robot.sphero.roll(90, Math.floor(Math.random() * 360));
+  work: function(my) {
+    console.log("Setting up collision detection.");
+    my.sphero.detectCollisions();
 
-    robot.salesforce.subscribe('/topic/SpheroMsgOutbound', function(data) {
-      var spheroName = data.sobject.Sphero_Name__c,
+    my.sphero.stop();
+    my.sphero.setRGB(0x00FF00);
+    my.sphero.roll(90, Math.floor(Math.random() * 360));
+
+    my.salesforce.subscribe('/topic/SpheroMsgOutbound', function(data) {
+      var name = data.sobject.Sphero_Name__c,
           bucks = data.sobject.Bucks__c;
 
-      cylon.Logger.info("Sphero: " + spheroName + ", data Bucks: " + bucks + ", SM_Id: " + data.sobject.Id);
-      //cylon.Logger.info("Sphero: #{ spheroName }, data Bucks: #{ bucks }, SM_Id: #{ data.sobject.Id }");
+      console.log("Sphero: " + name + ", data Bucks: " + bucks + ", SM_Id: " + data.sobject.Id);
 
-      robot.sphero.setRGB(0x00FF00);
-      robot.sphero.roll(90, Math.floor(Math.random() * 360));
+      my.sphero.setRGB(0x00FF00);
+      my.sphero.roll(90, Math.floor(Math.random() * 360));
     });
 
-    robot.sphero.on('collision', function() {
-      robot.sphero.setRGB(0x0000FF);
-      robot.sphero.stop();
-      var toSend = '{ "spheroName" :"' + robot.name + '", "bucks": "' + robot.totalBucks++ + '" }';
-      //var toSend = "{ \"spheroName\" :\"#{ robot.name }\", \"bucks\": \"#{ robot.totalBucks++ }\" }";
-      robot.salesforce.push('SpheroController', 'POST', toSend);
+    my.sphero.on('collision', function() {
+      var data = { spheroName: my.name, bucks: my.totalBucks++ };
+
+      my.sphero.setRGB(0x0000FF);
+      my.sphero.stop();
+
+      my.salesforce.push('SpheroController', 'POST', JSON.stringify(data));
     });
   }
-};
-
-cylon.robot(salesforceRobot);
-
-cylon.start();
+}).start();
